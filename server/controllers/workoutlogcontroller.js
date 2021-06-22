082ff38167e2779e19f7cdadeb87b0d8c46bbc16
Brad Titus <bltitus@me.com>
2021-06-22 00:25:22 -0400

    create, edit, search, and delete workouts

--- server/controllers/workoutlogcontroller.js
+++ server/controllers/workoutlogcontroller.js
@@ -1,13 +1,104 @@
 const Express = require("express");
 const router = Express.Router();
 let validateJWT = require("../middleware/validate-jwt");
+const workoutLog = require("../models/log");
 
 router.get('/practice', validateJWT, (req, res) => {
     res.send('Hey!! This is a practice route!')
 });
 
-router.get('/about', (req, res) => {
-    res.send('This is the about route, but I will not use an exclamation point because that seems excessive.')
+// New Log
+router.post("/create", validateJWT, async (req, res) => {
+    const { description, definition, result } = req.body.workoutLog; 
+    const id = req.user.id;
+    const logEntry = {
+        description,
+        definition,
+        result,
+        owner_id: id
+    }
+    try {
+        const newWorkout = await workoutLog.create(logEntry);
+        res.status(200).json(newWorkout)
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Get Logs
+
+router.get('/', async (req, res) => {
+    try {
+        const entries = await workoutLog.findAll();
+        res.status(200).json(entries);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Get Logs by User ID
+
+router.get("/mine", validateJWT, async (req, res) => {
+    const { id } = req.user;
+    try {
+        const userLogs = await workoutLog.findAll({
+            where: {
+                owner_id: id
+            }
+        });
+        res.status(200).json(userLogs);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Update a Workout Log Entry
+
+router.put("/update/:logId", validateJWT, async (req, res) => {
+    const { description, definition, result } = req.body.workoutLog;
+    const logId = req.params.log;
+    const userId = req.user.id;
+
+    const query = {
+        where: {
+            id: logId,
+            owner: userId
+        }
+    };
+
+    const updatedLog = {
+        description: description,
+        definition: definition,
+        result: result
+    };
+
+    try {
+        const update = await workoutLog.update(updatedLog, query);
+        res.status(200).json(update);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Delete a Workout Log Entry
+
+router.delete("/delete/:id", validateJWT, async (req, res) => {
+    const ownerId = req.user.id;
+    const workoutId = req.params.id;
+
+    try {
+        const query = {
+            where: {
+                id: workoutId,
+                owner: ownerId
+            }
+        };
+
+        await workoutLog.destroy(query);
+        res.status(200).json({ message: "Workout log removed" });
+    } catch (err) {
+        res.status(500).json({error: err.message });
+    }
 });
 
 module.exports = router;
---
+++ server/models/log.js
@@ -0,0 +1,22 @@
+const { DataTypes, INTEGER } = require("sequelize");
+const db = require("../db");
+
+const workoutLog = db.define("workoutLog", {
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    definition: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    result: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    owner_id: {
+        type: DataTypes.INTEGER
+    }
+});
+
+module.exports = workoutLog;
